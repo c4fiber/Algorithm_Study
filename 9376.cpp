6d510cae5dea109ea0a7fd29e02acd82dfad6350
c4fiber <qudcjf153@gmail.com>
2020-10-22 01:22:49 +0900

    Create 9376.cpp
    미완)

---
+++ 9376.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// int: 'xxxyyy' 상수로 나타냄
+int routeWayOut(char* map[], int xy, int openedDoors){
+    
+
+    return openedDoors + routeWayOut(map, xy + 1000, openedDoors)
+        + routeWayOut(map, xy - 1000, openedDoors)
+        + routeWayOut(map, xy + 1, openedDoors)
+        + routeWayOut(map, xy-1, openedDoors);
+}
+
+int main() {
+    int cases, height, width;
+    char map[100][100] = {};
+    cin >> cases;
+
+    for (int i=0; i<cases; i++){
+        cin >> height >> width;
+
+        for (int j=0; i<height; j++){
+            cin >> map[j];
+        }
+    }
+}
