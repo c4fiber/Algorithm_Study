47ce0fa5b1c6247a8ca4427b48a6c4e167b2c0b3
c4fiber <qudcjf153@gmail.com>
2021-08-02 21:40:56 +0900

    Create number_of_k.cpp
    https://programmers.co.kr/learn/courses/30/lessons/42748/
    
    + vector의 begin, end가 가르키는 부분을 알게 되었다. (end가 벡터 마지막 값이 아닌 그 다음 위치를 가르키는 게 충격적이였음)
    + iterator 헤더를 이용해 벡터를 출력하여 벡터가 제대로 잘리는지 확인하였음
    + 간결하게 적을 수 있었지만 begin, end, index 등 직관적으로 이해할 수 있도록 임의로 변수를 선언하여 이용하였음
    - 메모리 낭비가 심한게 아닌가 생각이 듬.
    - assign 연산이 추가되었기 때문에 자원소모가 더함.
    - 정렬부분을 직접 구현하지 않고 sort 함수를 사용함.

---
+++ programmers/number_of_k.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iterator>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> temp (array.size());
+    
+    for (vector<int> command : commands) {
+        int begin = command[0] - 1;
+        int end = command[1];
+        int index = command[2] - 1;
+
+        temp.clear();
+        temp.assign(array.begin() + begin, array.begin() + end);
+
+        // debug
+        copy(temp.begin(), temp.end(), ostream_iterator<int>(cout, "; "));
+        cout << endl;
+
+        sort(temp.begin(), temp.end());
+        
+        answer.push_back(temp[index]);
+    }
+    
+    
+    return answer;
+}
+
+int main() {
+    vector<int> array = {1, 5, 2, 6, 3, 7, 4};
+    vector<vector<int>> commands = {{2,5,3}, {4,4,1}, {1,7,3}};
+
+    vector<int> answer = solution(array, commands);
+
+    for (int i : answer) {
+        cout << i << endl;
+    }
+    return 0;
+}
